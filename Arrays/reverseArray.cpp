f42d8fa8c6b3825a78b220a3097b35a2ae73dbe2
vivekio07 <vi2005b@gmail.com>
2025-10-03 01:11:40 +0530

    Create reverseArray.cpp

---
+++ Arrays/reverseArray.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void reverseArray(int arr[], int n) {
+    int start = 0, end = n - 1;
+    while (start < end) {
+        // Swap elements
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    reverseArray(arr, n);
+
+    cout << "Reversed array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
