3962366461e05b4dc221f47fd3a1769581419929
vivekio07 <vi2005b@gmail.com>
2025-10-08 23:54:53 +0530

    Create sortingArray.cpp

---
+++ Arrays/sortingArray.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+/*
+    QUESTION:
+    Write a C++ program to sort an array of n integers in ascending order.
+    Use the Bubble Sort technique.
+
+    Example:
+    Input:  n = 5
+            arr[] = {5, 3, 1, 4, 2}
+    Output: 1 2 3 4 5
+*/
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        // Last i elements are already in place
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // swap arr[j] and arr[j+1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter " << n << " numbers: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    bubbleSort(arr, n);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
