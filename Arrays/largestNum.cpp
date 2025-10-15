93fd548a34eb784a45f98baeb7ac5fa45f92f4f0
vivekio07 <vi2005b@gmail.com>
2025-10-15 22:16:40 +0530

    Create largestNum.cpp

---
+++ Arrays/largestNum.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter " << n << " elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int largest = arr[0];
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+
+    cout << "Largest element is: " << largest << endl;
+    return 0;
+}
